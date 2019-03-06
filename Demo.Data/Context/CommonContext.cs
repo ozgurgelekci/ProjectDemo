5f5ff5853f1e2b7f3e8ebfd611de4405b340b73f
Özgür Gelekçi <ozgurgelekci@aof.anadolu.edu.tr>
2019-03-06 19:50:58 +0300

    Common Contex

---
+++ Demo.Data/Context/CommonContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data.Context
+{
+    public class CommonContext : DbContext
+    {
+        public CommonContext()
+            : base("name=DemoEntities")
+        {
+
+        }
+    }
+}
