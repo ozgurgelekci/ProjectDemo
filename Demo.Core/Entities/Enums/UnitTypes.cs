78975defee8d5dbaf5ffd6d6ae96487b238c2025
Özgür Gelekçi <ozgurgelekci@aof.anadolu.edu.tr>
2019-03-06 19:11:05 +0300

    LookupList unit types

---
+++ Demo.Core/Entities/Enums/UnitTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Core.Entities.Enums
+{
+    public struct UnitTypes
+    {
+        public const short GenelMudur = 1;
+        public const short Mudurluk = 2;
+        public const short DaireBaskanligi = 3;
+        public const short SubeMudurlugu = 4;
+    }
+}
