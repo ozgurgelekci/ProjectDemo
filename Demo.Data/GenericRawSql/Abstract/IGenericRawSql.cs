c4e2c47a2f1ed78286d7fdb0b03a0869f03bfb7d
Özgür Gelekçi <ozgurgelekci@aof.anadolu.edu.tr>
2019-03-06 21:58:36 +0300

    Generic raw sql implementation

---
+++ Demo.Data/GenericRawSql/Abstract/IGenericRawSql.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+
+namespace Demo.Data.GenericRawSql.Abstract
+{
+    public interface IGenericRawSql
+    {
+        /// <summary>
+        /// Harici sorgulamalar yapmak için kullanılır.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="t"></param>
+        /// <param name="sql"></param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        DbRawSqlQuery Execute(string connectionString, Type t, string sql, params object[] parameter);
+    }
+}
---
+++ Demo.Data/GenericRawSql/Concrete/GenericRawSql.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using Demo.Data.Context;
+using Demo.Data.GenericRawSql.Abstract;
+
+
+namespace Demo.Data.GenericRawSql.Concrete
+{
+    public class GenericRawSql : IGenericRawSql
+    {
+        private readonly CommonContext _context;
+
+        public GenericRawSql(CommonContext context)
+        {
+            _context = context;
+        }
+
+        public DbRawSqlQuery Execute(string connectionString, Type t, string sql, params object[] parameter)
+        {
+            _context.Database.Connection.ConnectionString = connectionString;
+            return _context.Database.SqlQuery(t, sql, parameter);
+        }
+    }
+}
