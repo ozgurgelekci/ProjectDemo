73fcc40482bb970a0b89c7d8cd01f3eb85b9b98a
Özgür Gelekçi <ozgurgelekci@aof.anadolu.edu.tr>
2019-03-06 21:13:51 +0300

    Generic repository concrete class

---
+++ Demo.Data/GenericRepository/Concrete/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Core.Entities.Abstract;
+using Demo.Data.Context;
+using Demo.Data.GenericRepository.Abstract;
+
+namespace Demo.Data.GenericRepository.Concrete
+{
+    public class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity> where TEntity : class, IEntity
+    where TContext : DbContext, new()
+    {
+        private readonly TContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(TContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter)
+        {
+            return _dbSet.Where(filter);
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return _dbSet.FirstOrDefault(filter);
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void AddRange(List<TEntity> entityList)
+        {
+            _dbSet.AddRange(entityList);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+                _dbSet.Attach(entity);
+            _dbSet.Remove(entity);
+        }
+    }
+}
