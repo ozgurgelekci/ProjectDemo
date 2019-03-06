0d2bc9b0b1c1f9c5cd8248c15ede6d7a40c3bbaa
Özgür Gelekçi <ozgurgelekci@aof.anadolu.edu.tr>
2019-03-06 22:27:17 +0300

    Unit of work design pattern implementation

---
+++ Demo.Data/UnitOfWork/Abstract/IUnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Demo.Core.Entities.Abstract;
+using Demo.Data.GenericRawSql.Abstract;
+using Demo.Data.GenericRepository.Abstract;
+
+namespace Demo.Data.UnitOfWork.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntity;
+        IGenericRawSql GetRawSql();
+
+        /// <summary>
+        /// Değişiklikleri kaydeder.
+        /// </summary>
+        /// <returns></returns>
+        int SaveChanges();
+
+        /// <summary>
+        /// Transaction başlatır.
+        /// </summary>
+        void BeginTransaction();
+
+        /// <summary>
+        /// Kayıt esnasında problem olmazsa transactionı bitirir.
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Kayıt esnasında problem olursa değişiklikleri geri alır.
+        /// </summary>
+        void RollBack();
+    }
+}
---
+++ Demo.Data/UnitOfWork/Concrete/UnitOfWork.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using Demo.Core.Entities.Abstract;
+using Demo.Data.Context;
+using Demo.Data.GenericRawSql.Abstract;
+using Demo.Data.GenericRepository.Abstract;
+using Demo.Data.GenericRepository.Concrete;
+using Demo.Data.UnitOfWork.Abstract;
+
+namespace Demo.Data.UnitOfWork.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DemoContext _demoContext;
+        private readonly CommonContext _commonContext;
+        private DbContextTransaction _dbContextTransaction = null;
+        private bool disposed = false;
+
+        public UnitOfWork(DemoContext demoContext, CommonContext commonContext)
+        {
+            Database.SetInitializer<DemoContext>(null);
+
+            if (_demoContext == null)
+            {
+                throw new ArgumentException("Context is null");
+            }
+
+            _demoContext = demoContext;
+            _commonContext = commonContext;
+        }
+
+        public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntity
+        {
+            return new GenericRepository<TEntity>(_demoContext);
+        }
+
+        public IGenericRawSql GetRawSql()
+        {
+            return new GenericRawSql.Concrete.GenericRawSql(_commonContext);
+        }
+
+        public int SaveChanges()
+        {
+            int affectedRow = 0;
+            try
+            {
+                _demoContext.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEntityValidationException)
+            {
+                var message = string.Empty;
+                foreach (var validationErrors in dbEntityValidationException.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        message += $"Property: {validationError.PropertyName}, Error: {validationError.ErrorMessage}" + Environment.NewLine;
+                    }
+                }
+
+                throw new Exception(message, dbEntityValidationException);
+            }
+
+            return affectedRow;
+        }
+
+        public void BeginTransaction()
+        {
+            _dbContextTransaction = _demoContext.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            _dbContextTransaction.Commit();
+        }
+
+        public void RollBack()
+        {
+            _dbContextTransaction.Rollback();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _demoContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
