b3967d2434509cdfe7b788301978d72714a0664c
Özgür Gelekçi <ozgurgelekci@aof.anadolu.edu.tr>
2019-03-07 18:49:55 +0300

    Ninject configuration for web mvc and web api

---
+++ Demo.IoC/Ninject/ApiDependencyResolver.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http.Dependencies;
+using Ninject;
+
+namespace Demo.IoC.Ninject
+{
+    public class ApiDependencyResolver : NinjectDependencyScope, IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public ApiDependencyResolver(IKernel kernel)
+        : base(kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectDependencyScope(_kernel.BeginBlock());
+        }
+    }
+}
---
+++ Demo.IoC/Ninject/MvcDependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http.Dependencies;
+using Ninject;
+using IDependencyResolver = System.Web.Mvc.IDependencyResolver;
+
+namespace Demo.IoC.Ninject
+{
+    public class MvcDependencyResolver : NinjectDependencyScope, IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public MvcDependencyResolver(IKernel kernel)
+        :base(kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectDependencyScope(_kernel.BeginBlock());
+        }
+    }
+}
